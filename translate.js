823ebe62f717b3d52f9f4980efa967617b11002e
htmlonlinet <95869708+htmlonlinet@users.noreply.github.com>
2022-12-11 10:38:43 -0500

    Create translate.js

---
+++ translate.js
@@ -0,0 +1,10 @@
+var script = document.createElement("script");
+script.src="https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit";
+document.createElement(script);
+function googleTranslateElementInit(){
+  new google.translate.TranslateElement(
+  {
+    pageLanguage:"en"
+  }
+ }
+};
