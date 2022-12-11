4720d20d3033d798762396ce46cb14a7db9970fc
htmlonlinet <95869708+htmlonlinet@users.noreply.github.com>
2022-12-11 10:44:32 -0500

    Update translate.js

--- translate.js
+++ translate.js
@@ -1,10 +1,9 @@
 var script = document.createElement("script");
 script.src="https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit";
 document.createElement(script);
-function googleTranslateElementInit(){
-  new google.translate.TranslateElement(
-  {
-    pageLanguage:"en"
-  }
- }
-};
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
+}
+var translate = document.createElement("div");
+translate.id = "google_translate_element";
+document.body.appendChild(translate);
