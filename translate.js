34e320c100e77c18dcbfe9535cbc44795e4df3e4
htmlonlinet <95869708+htmlonlinet@users.noreply.github.com>
2022-12-11 10:50:53 -0500

    Delete translate.js

--- translate.js
+++
@@ -1,6 +0,0 @@
-var script = document.createElement("script");
-script.src="https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit";
-document.createElement(script);
-function googleTranslateElementInit() {
-  new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
-}
